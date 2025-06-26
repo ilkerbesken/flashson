3743bacc4f605721130cf2a41f5890b5354b3411
ilkerbesken <ilkerbesken@gmail.com>
2025-06-26 22:04:49 +0200

    PWA ve UX iyileştirmeleri: welcome modal, tekrar db.json seçimi engellendi, responsive ve modern deneyim

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'flashcards-pwa-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/icon-192.png',
-  '/icon-512.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
